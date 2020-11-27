8adda845bf6675bf2a1305c05736d8f35d0b5231
BroadRey <62501467+BroadRey@users.noreply.github.com>
2020-11-27 20:47:21 +0300

    Add files via upload

---
+++ WeightOnTheMoon.java
@@ -0,0 +1,11 @@
+public class WeightOnTheMoon {
+    public static void main(String[] args) {
+        int weightOnEarth, weightOnMoon;
+
+        weightOnEarth = 65;
+        weightOnMoon = (int) (weightOnEarth * 0.17);
+
+        System.out.println("Мой вес на Земле: " + weightOnEarth);
+        System.out.println("Мой вес на Луне: " + weightOnMoon);
+    }
+}
