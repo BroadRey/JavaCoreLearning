725bc20a1a5d7eb5d30730ccf7c2e476d0a87150
BroadRey <62501467+BroadRey@users.noreply.github.com>
2020-12-12 14:31:17 +0300

    Add files via upload

---
+++ DemoPhonebook.java
@@ -0,0 +1,31 @@
+/*
+* Простейший автоматизированный телефонный справочник,
+* для корректной работы которого необходимо передать в качестве
+* аргумента командной строки имя абонента
+*/
+
+class DemoPhonebook {
+    public static void main(String[] args) {
+        String[][] numbers = {
+                {"Мария", "666-3434"},
+                {"Андрей", "357-274"},
+                {"Ольга", "354-777"},
+        };
+
+        if (args.length != 1) {
+            System.out.println("Использование: java DemoPhonebook <Имя>");
+        } else {
+            int iterator = 0;
+            for (String[] x : numbers) {
+                if (x[0].equals(args[0])) {
+                    System.out.println(x[1]);
+                    break;
+                }
+                iterator++;
+            }
+
+            if (iterator == numbers.length)
+                System.out.println("Элемент не найден!");
+        }
+    }
+}
